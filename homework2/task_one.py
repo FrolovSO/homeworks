86afc36aaee699bb0ea6b7465e8a66722729a0e9
Tesla32 <frolovso92@gmail.com>
2022-11-22 10:42:24 +0200

    Implemented first task

---
+++ homework2/task_one.py
@@ -0,0 +1,9 @@
+first_age = 27
+second_age = 43
+third_age = 38
+
+neighbors_years_sum = first_age + second_age + third_age
+average_neighbors_age = neighbors_years_sum / 3
+
+print(f'ОбЩий возрост соседей: {neighbors_years_sum}')
+print(f'Средний возрост соседей: {average_neighbors_age}')
